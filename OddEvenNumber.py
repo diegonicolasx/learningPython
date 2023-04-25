6aa9bcf5f9ac793685413ea773307ae8238f6385
diegonicolasx <130861778+diegonicolasx@users.noreply.github.com>
2023-04-24 21:10:21 -0400

    Create OddEvenNumber.py

---
+++ OddEvenNumber.py
@@ -0,0 +1,11 @@
+# Entrada de datos.
+n = int(input('Ingrese su número: '))
+resto = n % 2
+
+# Proceso.
+if (resto == 0):
+  
+# Salida.
+  print(f'El número ingresado {n} es par')
+else:
+  print(f'El número ingresado {n} es impar')
